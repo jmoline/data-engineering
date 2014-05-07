e1498f0d46a4d390e9ae296f9eb99eb3460e40dd
James W Moline <jmoline@gmail.com>
2014-05-07 09:33:50 -0400

    purchase model validations and associations

--- app/models/purchase.rb
+++ app/models/purchase.rb
@@ -1,2 +1,10 @@
 class Purchase < ActiveRecord::Base
+  belongs_to :customer
+  belongs_to :merchant
+  belongs_to :item
+
+  validates_presence_of :customer
+  validates_presence_of :merchant
+  validates_presence_of :item
+  validates_numericality_of :quantity, greater_than: 0, allow_nil: false
 end
--- test/models/purchase_test.rb
+++ test/models/purchase_test.rb
@@ -1,7 +1,41 @@
 require 'test_helper'
 
-class PurchaseTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+describe Purchase do
+  let(:purchase) { Purchase.new }
+
+  describe "validations" do
+    before do
+      purchase.valid?
+    end
+
+    it "validates that there is a customer" do
+      purchase.errors[:customer].must_equal ["can't be blank"]
+    end
+
+    it "validates that there is a merchant" do
+      purchase.errors[:merchant].must_equal ["can't be blank"]
+    end
+
+    it "validates that there is a item" do
+      purchase.errors[:item].must_equal ["can't be blank"]
+    end
+
+    describe "quantity" do
+      it "validates that there is a quantity" do
+        purchase.errors[:quantity].must_equal ["is not a number"]
+      end
+
+      it "validates that the quantity is greater than 0" do
+        purchase.quantity = -2
+        purchase.valid?
+        purchase.errors[:quantity].must_equal ["must be greater than 0"]
+      end
+
+      it "passes validation when quantity is present and greater than 0" do
+        purchase.quantity = 2
+        purchase.valid?
+        purchase.errors[:quantity].must_be_empty
+      end
+    end
+  end
 end
