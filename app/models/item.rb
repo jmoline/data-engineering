a234f26559f6e55c8e1a1102c555d431d81e8d35
James W Moline <jmoline@gmail.com>
2014-05-07 09:17:48 -0400

    item model validations and associations

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,2 +1,6 @@
 class Item < ActiveRecord::Base
+  has_many :purchases
+
+  validates_presence_of :description
+  validates_numericality_of :price, greater_than: 0, allow_nil: false
 end
--- test/models/item_test.rb
+++ test/models/item_test.rb
@@ -1,7 +1,33 @@
 require 'test_helper'
 
-class ItemTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+describe Item do
+  let(:item) { Item.new }
+
+  describe "validations" do
+    before do
+      item.valid?
+    end
+
+    it "validates that there is a description" do
+      item.errors[:description].must_equal ["can't be blank"]
+    end
+
+    describe "price" do
+      it "validates that there is a price" do
+        item.errors[:price].must_equal ["is not a number"]
+      end
+
+      it "validates that the price is greater than 0" do
+        item.price = -2.0
+        item.valid?
+        item.errors[:price].must_equal ["must be greater than 0"]
+      end
+
+      it "passes validation when price is present and greater than 0" do
+        item.price = 2
+        item.valid?
+        item.errors[:price].must_be_empty
+      end
+    end
+  end
 end
