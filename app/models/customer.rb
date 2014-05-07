4b51ff3ec2c801579401caf26121ad38e7809b4f
James W Moline <jmoline@gmail.com>
2014-05-07 18:20:17 -0400

    adding a find_or_create method to the customer model

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -2,4 +2,11 @@ class Customer < ActiveRecord::Base
   has_many :purchases
 
   validates_presence_of :name
+
+  def self.find_or_create(attributes={})
+    unless instance = self.where(name: attributes[:name]).first
+      instance = self.create(attributes)
+    end
+    instance
+  end
 end
--- test/models/customer_test.rb
+++ test/models/customer_test.rb
@@ -12,4 +12,26 @@ describe Customer do
       customer.errors[:name].must_equal ["can't be blank"]
     end
   end
+
+  it "should respond to find_or_create" do
+    Customer.must_respond_to :find_or_create
+  end
+
+  describe "find_or_create" do
+    it "should return an existing record when passed an existing name" do
+      Customer.stub(:where, [Customer.new(name: "William Murdoch")]) do
+        customer = Customer.find_or_create(name: "Testy McTesterson")
+        customer.name.must_equal "William Murdoch"
+      end
+    end
+
+    it "should return a new record when passed a new name" do
+      Customer.stub(:where, []) do
+        Customer.stub(:create, Customer.new(name: "Testy McTesterson")) do
+          customer = Customer.find_or_create(name: "William Murdoch")
+          customer.name.must_equal "Testy McTesterson"
+        end
+      end
+    end
+  end
 end
