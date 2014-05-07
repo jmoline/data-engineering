acd5d51a53db8d43d8a919843e05805a9498c8ad
James W Moline <jmoline@gmail.com>
2014-05-07 09:03:29 -0400

    merchant model validations and associations

--- app/models/merchant.rb
+++ app/models/merchant.rb
@@ -1,2 +1,6 @@
 class Merchant < ActiveRecord::Base
+  has_many :purchases
+
+  validates_presence_of :name
+  validates_presence_of :address
 end
--- test/models/merchant_test.rb
+++ test/models/merchant_test.rb
@@ -1,7 +1,17 @@
 require 'test_helper'
 
-class MerchantTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+describe Merchant do
+  let(:merchant) { Merchant.new }
+
+  before do
+    merchant.valid?
+  end
+
+  it "validates that there is a name" do
+    merchant.errors[:name].must_equal ["can't be blank"]
+  end
+
+  it "validates that there is an address" do
+    merchant.errors[:address].must_equal ["can't be blank"]
+  end
 end
